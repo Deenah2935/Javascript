843fa0e3cfd75c3662c8493234bdb4b034f700cd
Deenah2935 <deenah2935.beai25@chitkara.edu.in>
2026-02-02 20:11:00 +0530

    Organized files

--- Exponential.js
+++
@@ -1,5 +0,0 @@
-let a=parseFloat(prompt("Enter the number"));
-let square=a**2;
-let cube=a**3;
-console.log("Square is:", square);
-console.log("Cube is:", cube);
--- remainder.js
+++
@@ -1,4 +0,0 @@
-let a=parseFloat(prompt("Enter the number"));
-let b=parseFloat(prompt("Enter the number"));
-let remainder=a%b;
-console.log("Remainder ")
